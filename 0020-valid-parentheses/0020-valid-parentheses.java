5bb1bd65eb88f363722d9f4e629dd4d3a91dd6a7
peethalasharmilaakanksha <peethalasharmilaakanksha@gmail.com>
2026-03-29 20:56:42 +0530

    Time: 4 ms (41.27%), Space: 43.2 MB (66.55%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0; i<s.length() ; i++){
+
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                st.push(s.charAt(i));
+            }
+            else{
+                if(st.empty()) return false;
+                char ch = st.peek();
+                st.pop();
+                if((ch=='(' && s.charAt(i)==')') || (ch=='[' && s.charAt(i)==']') || (ch=='{' && s.charAt(i)=='}')){
+                    continue;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        
+        return st.isEmpty();
+    }
+}
